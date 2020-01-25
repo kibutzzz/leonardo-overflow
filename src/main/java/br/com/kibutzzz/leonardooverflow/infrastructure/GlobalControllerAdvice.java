efe58396fc4270e2fc19c8c5a62a42523fb532ba
Leonardo Meinerz Ramos <leonardo.meinerzramos28@gmail.com>
2020-01-25 19:58:40 -0300

    add Exception handler for Unreadable request body

--- src/main/java/br/com/kibutzzz/leonardooverflow/infrastructure/GlobalControllerAdvice.java
+++ src/main/java/br/com/kibutzzz/leonardooverflow/infrastructure/GlobalControllerAdvice.java
@@ -5,6 +5,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -22,6 +23,12 @@ public class GlobalControllerAdvice {
     public ResponseEntity<ApiErrorResponse> handleGenericException(Exception ex) {
         ApiErrorResponse errorResponse = new ApiErrorResponse(ex.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ApiErrorResponse> handleInvalidFormatException(HttpMessageNotReadableException ex) {
+
+        return ResponseEntity.badRequest().body(new ApiErrorResponse(ex.toString()));
     }
 
     @ExceptionHandler(ApiException.class)
